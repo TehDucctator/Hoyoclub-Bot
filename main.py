882d89c0a8ec993c48f65afee610699c17209842
Ducc <ducky543210@gmail.com>
2022-10-22 14:36:12 -0400

    Update gitignore and main.py and add responses.py
    add pycache to gitignore, Update main.py to use responses.py to
    respond to a list of possible good mornings with a list of possible
    responses

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+import os, random
 import discord
-import os
+from discord.ext import tasks
+import responses
 
 intents = discord.Intents.default()
 intents.message_content = True
 
 client = discord.Client(intents=intents)
 
+status = iter(["with coffins", "with Qiqi", "with fire, shh"]) # statuses to go through
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
+    change_status.start() # start loop to change status
+
+@tasks.loop(minutes=10) # changes status every 10 minutes
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
 
 @client.event
 async def on_message(message):
-    if message.author == client.user:
+    if message.author == client.user: # prevents reading its own messages
         return
 
-    if message.content.lower().startswith('good morning'):
-        await message.channel.send('Good morning!')
+    text = message.content.lower() # gets lowered message content
+    if text.startswith(responses.gm_messages): # checks for good mornings
+        await message.channel.send(random.choice(responses.gm_responses)) # responds with a gm
 
 client.run(os.getenv('TOKEN'))
---
+++ responses.py
@@ -0,0 +1,9 @@
+gm_messages = tuple(["good mornin", "gm", "mornin"]) # good morning messages to respond to
+
+gm_responses = ["Good morning!",
+                "Good morning! Glad to see you aren't in a coffin today, if you ever need one you know where to look!",
+                "Well are those who rise in the early morn, while those late to bed I shall forewarn~",
+                "Good morning! Death is inevitable, but don't worry, we’re doing a 50% coffin discount today!",
+                "Good morning! Today is another day closer to another coffin purchase.",
+                "Good morning! Wanna come over for tea?",
+                "Good morning! I wonder how many clients we will have today."]
