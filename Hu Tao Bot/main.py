5a24533bae3c1e64bd12849b0b222477061faf56
Ducc <ducky543210@gmail.com>
2022-10-24 22:30:36 -0400

    Move main.py and responses.py into Hu Tao Bot folder

--- Hu Tao Bot/main.py
+++ Hu Tao Bot/main.py
@@ -8,15 +8,19 @@ intents.message_content = True
 
 client = discord.Client(intents=intents)
 
-status = iter(["with coffins", "with Qiqi", "with fire, shh"]) # statuses to go through
+status = ["with coffins", "with Qiqi", "with fire, shh", "with ghosts", "with Boo Tao"] # statuses to go through
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     change_status.start() # start loop to change status
 
-@tasks.loop(minutes=10) # changes status every 10 minutes
+@tasks.loop(minutes=10, count=len(status)) # changes status every 10 minutes
 async def change_status():
-    await client.change_presence(activity=discord.Game(next(status)))
+    await client.change_presence(activity=discord.Game(status[change_status.current_loop]))
+
+@change_status.after_loop
+async def restart_status(): # restart change_status
+    change_status.restart()
 
 @client.event
 async def on_message(message):
@@ -27,4 +31,5 @@ async def on_message(message):
     if text.startswith(responses.gm_messages): # checks for good mornings
         await message.channel.send(random.choice(responses.gm_responses)) # responds with a gm
 
-client.run(os.getenv('TOKEN'))
+if __name__ == "__main__":
+    client.run(os.getenv('TOKEN'))
